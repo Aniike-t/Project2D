cab520f9c63911eed668b7f42381ec33823df7fb
Aniike-t <72291316+Aniike-t@users.noreply.github.com.>
2024-09-16 14:18:22 +0530

    +Book +Hello World

---
+++ app1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// Comment goes here 
+    /// </summary>
+    
+    internal class Program
+    {
+        /// <summary>
+        /// Comment goes here 
+        /// </summary>
+        /// <param name="args">Command line args</param>
+        
+        static void Main(string[] args)
+        {
+            // Print supportive message
+            Console.WriteLine("Hello, World");
+        }
+    }
+}
